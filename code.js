2446ecb353336aef0639a6f6117c55fd75d05c17
Tobias Braendle <80142161+Tobi-art@users.noreply.github.com>
2021-03-11 14:18:41 +0900

    Dlt function for i=0; minor code&comment cleanup

--- code.js
+++ code.js
@@ -1,7 +1,8 @@
-let intArray = [];
 let arrQna = [];
+let score = 0;
+let i = 0;
 
-///firebaseのデータを取得し、配列に入れます//////
+//firebaseのデータを取得し、配列に入れます
 const ref = firebase.database().ref('Words')
 ref.on('value', getKey)
 
@@ -9,9 +10,9 @@ function getKey(data) {
     var words = data.val();
     var keys = Object.keys(words)
 
-    for (let i = 0; i < keys.length; i++) {
+    for (let j = 0; j < keys.length; j++) {
         intArray = []
-        let k = keys[i];
+        let k = keys[j];
 
         let v = words[k].Question;
         let w = words[k].Answer1;
@@ -23,11 +24,7 @@ function getKey(data) {
         arrQna.push(intArray)
     }
 
-    ////配列のデータを表示します。////////////////////
-
-    let i = 0;
-    let score = 0;
-
+    //配列のデータを表示します。
     function q(i) {
         $('#q').text(arrQna[i][0]);
         $('#ans1').text(arrQna[i][1]);
@@ -36,41 +33,63 @@ function getKey(data) {
     };
     q(i);
 
-    ////回答を選んだら、正しいかどうか表示します。////////////
+    //一番最初に表示されたデータの削除機能。69行目の関数は、$('[name=ans]')にクリックしてから行動します。
+    const dlt = firebase.database().ref('Words') //ここは７〜１６行目と同じなのでgetKey関数を使いたいですが、なかなか呼び出しできません…
+    dlt.on('value', getKeys)
 
+    function getKeys(data) {
+        var scores = data.val();
+        var keys = Object.keys(scores)
+        var k = keys[i];
+        $("#delete").on('click', function() {
+            if (i === 0) {
+                alert(`"${arrQna[i][0]}"を削除しますか。消してから復旧できません！`)
+                firebase.database().ref(`Words/${k}`).remove()
+            }
+        });
+    }
+
+    //回答を選んだら、正しいかどうか表示します。
     $('[name=ans]').on('click', function() {
         if ($(this).val() == arrQna[i][4]) {
             alert('正解です。');
             score++;
         } else {
-            alert(`違います！正解は選択肢${arrQna[i][4]}です。`);
+            alert(`違います！正解は ${arrQna[i][4]}) です。`);
         };
-        /////次の質問に進みます///////////////////////
+        //次の質問に進みます
         i++;
-        //////////一回全ての質問が出てからスコアを表示します////////////
+        //一回全ての質問が出てからスコアを表示します
         if (i < arrQna.length) {
             q(i);
-            // ↑ ここの時点で出てきたデータダケ消したいです。
+
+            //↑↑ ここの時点で出てきたデータのみ消したいです。
             ////////////////データ削除//////////////////
-            const dlt = firebase.database().ref('Words')
+            const dlt = firebase.database().ref('Words') //ここは７〜１６行目と同じなのでgetKey関数を使いたいですが、なかなか呼び出しできません…
             dlt.on('value', getKeys)
 
             function getKeys(data) {
                 var scores = data.val();
                 var keys = Object.keys(scores)
-                var k = keys[i]; ///
-                ///ここにalert(arrQna[i][0])と記述すると、次の質問のみ表示されます。
+                var k = keys[i];
+
                 $("#delete").on('click', function() {
-                    ///ここにalert(arrQna[i][0])と記述すると、次の質問がi回表示されます？？？？？            
-                    alert(`"${arrQna[i][0]}"を削除しますか。消してから復旧できません！`)
-                        // firebase.database().ref(`Words/${k}`).remove()
+                    if (i != 1) {
+                        alert('まだバグ対応中なので現時点で削除できません。')
+                            //このアラートは何度もでます！？（i回）削除機能を有効にすると、複数の単語が消えてしまします。
+                    }　
+                    else {
+                        alert(`"${arrQna[i][0]}"を削除しますか。消してから復旧できません！`)
+                        firebase.database().ref(`Words/${k}`).remove()
+                    }
                 });
             }
-        } else {
+
+        } else { //全ての質問が１回でたら、スコアを表示します。
             $('main').hide(200);
             $('#delete').hide(200);
             setTimeout(function() {
-                $('#score').text(`今回の正解率は${(score/arrQna.length)*100}%です。`);
+                $('#score').text(`今回の正解率は${Math.floor((score/arrQna.length)*100)}%です。`);
             }, 500);
             $('aside').show(200);
         }
--- testdata.js
+++ testdata.js
@@ -1,47 +1,47 @@
 firebase.database().ref('Words').push({
     Question: `htmlのリンクタグ`,
-    Answer1: `<href>`,
-    Answer2: `<a>`,
-    Answer3: `<link>`,
+    Answer1: `1) <href>`,
+    Answer2: `2) <a>`,
+    Answer3: `3) <link>`,
     Correct: 2
 });
 
 firebase.database().ref('Words').push({
     Question: `画像タグ`,
-    Answer1: `<igm>`,
-    Answer2: `<src>`,
-    Answer3: `<img>`,
+    Answer1: `1) <igm>`,
+    Answer2: `2) <src>`,
+    Answer3: `3) <img>`,
     Correct: 3
 });
 
 firebase.database().ref('Words').push({
     Question: `array`,
-    Answer1: `配列`,
-    Answer2: `矢印`,
-    Answer3: `関数の省略`,
+    Answer1: `1) 配列`,
+    Answer2: `2) 矢印`,
+    Answer3: `3) 関数の省略した書き方`,
     Correct: 1
 });
 
 firebase.database().ref('Words').push({
     Question: `var`,
-    Answer1: `変数の宣言。値を変更できる。`,
-    Answer2: `関数の宣言。値を変更できない。`,
-    Answer3: `関数の宣言。再宣言できない。`,
+    Answer1: `1) 変数の宣言。値を変更できる。`,
+    Answer2: `2) 関数の宣言。値を変更できない。`,
+    Answer3: `3) 関数の宣言。再宣言できない。`,
     Correct: 1
 });
 
 firebase.database().ref('Words').push({
     Question: `return`,
-    Answer1: `ポップアップで表示されます。`,
-    Answer2: `コンソルに表示されます。`,
-    Answer3: `値が関数外にアクセス可能になります。`,
+    Answer1: `1) ポップアップで表示されます。`,
+    Answer2: `2) コンソルに表示されます。`,
+    Answer3: `3) 値が関数外にアクセス可能になります。`,
     Correct: 3
 });
 
 firebase.database().ref('Words').push({
     Question: `alert`,
-    Answer1: `コンソルに表示されます。`,
-    Answer2: `ポップアップで表示されます。`,
-    Answer3: `値が関数外にアクセス可能になります。`,
+    Answer1: `1) コンソルに表示されます。`,
+    Answer2: `2) ポップアップで表示されます。`,
+    Answer3: `3) 値が関数外にアクセス可能になります。`,
     Correct: 2
 });
